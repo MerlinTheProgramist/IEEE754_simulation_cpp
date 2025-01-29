143a34db85bfad962a0f8377d423c5e2533a7c2e
Merlin <kvasjir@proton.me>
2025-01-29 23:19:24 +0100

    almost all ops

--- floating.cpp
+++ floating.cpp
@@ -1,57 +1,63 @@
-#include <atomic>
 #include <cassert>
+#include <cmath>
 #include <cstdint>
-#include <iomanip>
-#include <ios>
 #include <iostream>
 #include <limits>
 
 #include "floating.hpp"
 
-#define ASSERT(expr, ...)   if(!(expr)){ \
-                              printf(__VA_ARGS__); \
-                            }
-#define ASSERT_EQ(left, right, ...) if((left)!=(right)){ \
-                                      std::cout << (left) << " != " << (right) << '\n'; \
-                                      std::cout << __VA_ARGS__ << std::endl; \
-                                    }
-
-// enum class IEEE754Type{
-//   SignalingNan,
-//   QuietNan,
-//   NegativeInfinity,
-//   NegativeZero,
-//   PositiveInfinity,
-//   PositiveZero,
-//   NegativeNormalized,
-//   PositiveNormalized,
-//   Denormalized
-// };
-
-
-
-
-const Float32 
-    ZERO{Float32{0}},
-    INFTY{pack(IEEEFields{false, bit_mask<int32_t>(Float32::EXPONENT_SIZE), 0})},
-    NEG_INFTY{neg(INFTY)},
-    NaN{pack(IEEEFields{false, bit_mask<int32_t>(Float32::EXPONENT_SIZE), 1})};
-
-
-constexpr bool is_nan(const Float32 f){
-  auto a = unpack(f);
-  return a.exponent==bit_mask<int32_t>(Float32::EXPONENT_SIZE) && a.mantissa != 0;
+template<typename T> 
+constexpr bool EQ (const T a, const T b){return a==b;}
+constexpr bool EQ (const float a, const float b){
+  return std::bit_cast<uint32_t>(a)==std::bit_cast<uint32_t>(b) 
+      || ((std::abs(a - b) <= std::numeric_limits<float>::epsilon() * std::max(std::abs(a), std::abs(b))))
+      || ((std::abs(b - a) <= std::numeric_limits<float>::epsilon() * std::max(std::abs(a), std::abs(b))));
 }
-// bool is_sNaN(Float32 f){
-//   auto a = unpack(f);
-//   return a.exponent==bit_mask<uint32_t>(EXPONENT_SIZE) && a.mantissa != 0 && ;
-// }
-constexpr bool is_inf(Float32 f){
- auto a = unpack(f);
-  return a.exponent==bit_mask<int32_t>(Float32::EXPONENT_SIZE) && a.mantissa == 0;
+constexpr bool EQ (const Float32 a, const Float32 b){
+  return EQ((float)a, (float)b);
 }
-constexpr bool is_zero(Float32 f){
-  return unpack(f).mantissa==0;
+
+#define RETURN_IF_A_B_BOTH(a, b, only_a, only_b, both) \
+  if((a)){ \
+    if((b)) \
+      return both; \
+    return (only_a); \
+  }else if((b)){ \
+    return (only_b); \
+  }
+
+constexpr bool is_nan(const Float32 a){
+  // max exponent && mantissa != 0
+  return ((a.bits >> Float32::EXPONENT_OFFSET) & bit_mask<uint32_t>(Float32::EXPONENT_SIZE)) == bit_mask<uint32_t>(Float32::EXPONENT_SIZE)
+      && (a.bits & bit_mask<uint32_t>(Float32::MANTISSA_SIZE)) != 0;
+}
+constexpr bool is_signaling(const Float32 a){
+  return a == Float32::SNaN;
+}
+constexpr bool is_inf(Float32 a){
+  // max exponent && mantissa = 0
+  return ((a.bits >> Float32::EXPONENT_OFFSET) & bit_mask<uint32_t>(Float32::EXPONENT_SIZE)) == bit_mask<uint32_t>(Float32::EXPONENT_SIZE)
+      && (a.bits & (bit_mask<uint32_t>(Float32::MANTISSA_SIZE))) == 0;
+}
+constexpr bool is_zero(Float32 a){
+  return (a.bits & bit_mask<uint32_t>(Float32::MANTISSA_SIZE + Float32::EXPONENT_SIZE)) == 0;
+}
+constexpr bool is_sign_minus(const Float32 a){
+  return a.bits&(1<<Float32::SIGN_OFFSET);
+}
+constexpr bool is_normal(const Float32 a){
+  if(is_nan(a) || is_inf(a))
+    return false;
+  return !is_subnormal(a);
+}
+constexpr bool is_subnormal(const Float32 a){
+  return ((a.bits >> Float32::EXPONENT_OFFSET) & bit_mask<uint32_t>(Float32::EXPONENT_SIZE)) == 0;
+}
+constexpr bool is_finite(const Float32 a){
+  return !is_inf(a);
+}
+constexpr bool total_order_mag(const Float32 a, const Float32 b){
+  return total_order(abs(a), abs(b));
 }
 
 constexpr Float32 int_to_float(int32_t n){
@@ -96,8 +102,243 @@ constexpr Float32 add(const Float32 fa, const Float32 fb){
     // they are both positive:
     if(is_inf(fa) || is_inf(fb))
       return Float32::MAX;
+
+    auto a = unpack(fa);
+    auto b = unpack(fb);
+
+    const uint8_t sign = 0;
+
+	//adjust smaller number and simply add their mantisa
+	//then adjust result
+	uint16_t exponent{};
+	uint32_t to_shift{};
+	uint32_t mantissa{};
+	if(a.exponent < b.exponent)
+	{
+		exponent = b.exponent; 
+		to_shift = b.exponent - a.exponent;
+		mantissa  = (a.mantissa >> to_shift) + b.mantissa;
+		if(to_shift >= Float32::TOTAL_SIZE)
+			mantissa = b.mantissa;
+	}
+	else
+	{
+		exponent = a.exponent; 
+		to_shift = a.exponent - b.exponent;
+		mantissa  = (b.mantissa >> to_shift) + a.mantissa;
+		if(to_shift >= 32)
+			mantissa = a.mantissa;
+	}
+	while(mantissa > bit_mask<uint32_t>(Float32::MANTISSA_SIZE+1)) //24 bits
+	{
+		exponent++;
+		mantissa >>= 1;
+	} 
+
+	const Float32 t = pack({sign, exponent, mantissa});
+	if(is_nan(t))
+		return sign ? Float32::MIN : Float32::MAX;
+
+	return t;
+    
 }
 
+
+constexpr Float32 sub(const Float32 fa, const Float32 fb){
+	if(is_nan(fa))
+		return fa;
+	if(is_nan(fb))
+		return fb;
+
+	if(is_sign_minus(fb))
+		return add(fa, neg(fb));
+	if(is_sign_minus(fa))
+		return neg(add(neg(fa), fb));
+
+
+	if(is_zero(fa)){
+	  if(is_zero(fb))
+	    return Float32::ZERO;
+	  return neg(fb);
+	}
+	if(is_zero(fb))
+	  return fa;
+
+	if(is_inf(fa)){
+	  if(is_inf(fb))
+	    return neg(Float32::QNaN);
+	  return Float32::INFTY;
+	}
+	if(is_inf(fb))
+	  return Float32::NEG_INFTY;
+	
+
+	auto a = unpack(fa);
+	auto b = unpack(fb);
+
+	const bool sign = qls(fa, fb);
+
+	uint16_t exponent{}; 
+	uint32_t to_shift{}; 
+	uint64_t a_mantisa = a.mantissa; 
+	uint64_t b_mantisa = b.mantissa; 
+	uint64_t mantissa{};
+
+  const size_t PREC_SHIFT = 40;
+	
+	a_mantisa <<= PREC_SHIFT;
+	b_mantisa <<= PREC_SHIFT;
+
+	if(!sign)
+	{
+		exponent = a.exponent;
+		to_shift = a.exponent - b.exponent;
+		mantissa  = a_mantisa - (b_mantisa >> to_shift);
+
+		if(to_shift >= sizeof(mantissa)*8)
+			mantissa = a_mantisa - 1;
+		
+	}
+	else
+	{
+		exponent = b.exponent;
+		to_shift = b.exponent - a.exponent;
+		mantissa  = b_mantisa - (a_mantisa >> to_shift);
+		if(to_shift >= sizeof(mantissa)*8)
+			mantissa = b_mantisa - 1;
+	}
+
+	if(mantissa == 0)
+		return 0;
+
+	while(mantissa > (bit_mask<uint64_t>(Float32::MANTISSA_SIZE+1) << PREC_SHIFT)) //24 bits
+	{
+		exponent++;
+		mantissa >>= 1;
+	} 
+	
+	while(mantissa < (Float32::MANTISSA_LEADING_1 << PREC_SHIFT)) //24 bits
+	{
+		exponent--;
+		mantissa <<= 1;
+	} 
+	
+	if(mantissa == 0)
+		return 0;
+	mantissa >>= PREC_SHIFT;
+	
+	const Float32 t = pack({sign,  exponent, static_cast<uint32_t>(mantissa)});
+	if(is_nan(t))
+		return sign ? Float32::MIN : Float32::MAX;
+
+	return t;
+} 
+
+constexpr Float32 div(const Float32 fa, const Float32 fb){
+	if(is_nan(fa))
+		return fa;
+	if(is_nan(fb))
+		return fb;
+	
+	const bool sign = is_sign_minus(fa) ^ is_sign_minus(fb);
+	
+	RETURN_IF_A_B_BOTH(is_inf(fa), is_inf(fb), 
+      	            sign?Float32::NEG_INFTY:Float32::INFTY, 
+      	            sign?Float32::NEG_ZERO:Float32::ZERO, 
+      	            neg(Float32::QNaN))
+	RETURN_IF_A_B_BOTH(is_zero(fa), is_zero(fb), 
+      	            sign?Float32::NEG_ZERO:Float32::ZERO, 
+      	            sign?Float32::NEG_INFTY:Float32::INFTY,
+      	            neg(Float32::QNaN))
+	
+	IEEEFields a = unpack(fa);
+	IEEEFields b = unpack(fb);
+
+	a.sign_bit ^= b.sign_bit;
+
+	/*
+		shifted 40 to increase precision of result
+		later res is shifted 40 to the right
+	*/
+  const size_t PREC_SHIFT = 40;
+	uint64_t num = static_cast<uint64_t>(a.mantissa) << PREC_SHIFT;
+	uint64_t div = b.mantissa;
+	uint64_t res = num / div;
+
+	res <<= Float32::MANTISSA_SIZE;
+
+	/*
+		subtracting exponents so BIAS has to be added
+	*/
+	a.exponent = a.exponent - b.exponent + Float32::BIAS;
+
+	if(res == 0)
+		return a.sign_bit ? Float32::MIN : Float32::MAX;
+
+	while((res & (1ul<<(8*sizeof(res)-1))) == 0)
+	{
+		res <<= 1;
+		a.exponent--;
+	}
+	
+	a.mantissa = res >> PREC_SHIFT;
+	const Float32 result = pack(a);
+	if(is_nan(result))
+		return a.sign_bit?Float32::MIN:Float32::MAX;
+
+	return result;
+  
+}
+constexpr Float32 sqrt(const Float32 fa){
+  if(is_zero(fa) || is_nan(fa))
+    return fa;
+  if(is_sign_minus(fa))
+    return neg(Float32::QNaN);
+  if(is_inf(fa)) // negative 0 is nan
+    return fa;
+
+  auto a = unpack(fa);
+
+  /*
+	sqrt(mant) can be computed as integer
+
+	in case of odd number we have to multiply mantisa by 2 first, then take square root
+	because 2^(odd / 2) will lead to 2^(even + 1/2) = 2^even * sqrt(2) 
+	sqrt(2 * mantisa)  = sqrt(mantisa) * sqrt(2)
+	*/
+
+	a.exponent -= Float32::BIAS;
+	const bool even = a.exponent%2==0;
+	a.exponent = static_cast<int16_t>(a.exponent) >> 1;
+
+	a.exponent += Float32::BIAS;
+
+	uint64_t mantissa = a.mantissa;
+	mantissa = std::sqrt(mantissa << (40 - even));
+	mantissa >>= Float32::EXPONENT_SIZE;
+	a.mantissa = mantissa;
+
+	return pack(a);
+}
+
+constexpr Float32 fused_mul_add(const Float32 x, const Float32 y, const Float32 z){
+  return add(mul(x,y), z);
+}
+
+constexpr cls float_class(const Float32 a){
+  using enum cls;
+  if(is_nan(a))
+    return qNan;
+  if(is_signaling(a))
+    return sNan;
+  if(is_inf(a))
+    return is_sign_minus(a)?nInf:pInf;
+  if(is_zero(a))
+    return is_sign_minus(a)?nZr:pZr;
+  if(is_normal(a))
+    return is_sign_minus(a)?nNrm:pNrm;
+  return is_sign_minus(a)?nSbn:pSbn;
+}
 
 /*
   1.0011b * 101.001b
@@ -113,12 +354,17 @@ constexpr Float32 add(const Float32 fa, const Float32 fb){
 
     if((is_inf(fa) && is_zero(fb)) 
     || (is_zero(fa) && is_inf(fb)))
-      return NaN;
+      return neg(Float32::QNaN);
 
+    if(is_zero(fa))
+      return fa;
+    if(is_zero(fb))
+      return fb;
+    
     // sign remains only if sign is diffrent
     const bool sign_bit = (fa.bits ^ fb.bits) >> Float32::SIGN_OFFSET;
     if(is_inf(fa) || is_inf(fb))
-      return Float32{(static_cast<uint32_t>(sign_bit) << Float32::SIGN_OFFSET) | INFTY.bits};
+      return Float32{(static_cast<uint32_t>(sign_bit) << Float32::SIGN_OFFSET) | Float32::INFTY.bits};
     
     IEEEFields a = unpack(fa);
     IEEEFields b = unpack(fb);
@@ -128,7 +374,7 @@ constexpr Float32 add(const Float32 fa, const Float32 fb){
       mantissas are mulilied and lower bits are discarded via shift
     */
       
-    int16_t exponent = a.exponent + b.exponent - Float32::BIAS;
+    uint16_t exponent = a.exponent + b.exponent - Float32::BIAS;
     uint64_t mantissa = static_cast<uint64_t>(a.mantissa)
                       * static_cast<uint64_t>(b.mantissa);
 
@@ -137,16 +383,17 @@ constexpr Float32 add(const Float32 fa, const Float32 fb){
     // normalization
     if(mantissa != 0){
       // check if mantissa is greater than its maximum
-      while(mantissa > bit_mask<uint64_t>(Float32::MANTISSA_SIZE)){
+      while(mantissa > bit_mask<uint64_t>(Float32::MANTISSA_SIZE+1)){
         exponent += 1;
         mantissa >>= 1;
       }
       // check if mantissa is less than its minimum
-      while(mantissa < 0x800000){
+      while(mantissa < Float32::MANTISSA_LEADING_1){
         exponent -= 1;
         mantissa <<= 1;
       }
     }
+    mantissa += 1;
 
     const Float32 result = pack(IEEEFields{sign_bit,exponent,static_cast<uint32_t>(mantissa)});
 
@@ -163,9 +410,9 @@ constexpr Float32 scaleB(const Float32 x, const int32_t N){
   if(N==0 || is_inf(x) || is_nan(x) || is_zero(x))
     return x;
 
-  if(N >= Float32::BIAS)
+  if(N >= static_cast<int32_t>(Float32::BIAS))
     return Float32::MAX;
-  if(N <= -Float32::BIAS)
+  if(N <= -static_cast<int32_t>(Float32::BIAS))
     return Float32::MIN;
   
   auto a = unpack(x);
@@ -175,8 +422,6 @@ constexpr Float32 scaleB(const Float32 x, const int32_t N){
 }
 
 constexpr Float32 neg(const Float32 x){
-  if(is_nan(x))
-    return x;
   return Float32{x.bits ^ (1<<(Float32::SIGN_OFFSET))};
 }
 constexpr Float32 abs(const Float32 x){
@@ -205,7 +450,8 @@ constexpr bool qne(const Float32 a, const Float32 b){
   return a.bits == b.bits;
 }
 constexpr bool qgr(const Float32 a, const Float32 b){
-  if(is_nan(a) || is_nan(b))
+  if(is_nan(a) || is_nan(b)
+  ||(is_zero(a) && is_zero(b)))
     return false;
 
   const bool sign_a = a.bits >> Float32::SIGN_OFFSET;
@@ -224,9 +470,11 @@ constexpr bool qgr(const Float32 a, const Float32 b){
 constexpr bool qge(const Float32 a, const Float32 b){
   if(is_nan(a) || is_nan(b))
     return false;
+  if(is_zero(a) && is_zero(b))
+    return true;
 
-  const bool sign_a = a.bits >> Float32::SIGN_OFFSET;
-  const bool sign_b = b.bits >> Float32::SIGN_OFFSET;
+  const bool sign_a = is_sign_minus(a);
+  const bool sign_b = is_sign_minus(b);
 
   // both positive
   if(!sign_a && !sign_b)
@@ -239,7 +487,8 @@ constexpr bool qge(const Float32 a, const Float32 b){
   return !sign_a && sign_b;
 }
 constexpr bool qls(const Float32 a, const Float32 b){
-  if(is_nan(a) || is_nan(b))
+  if(is_nan(a) || is_nan(b) 
+  ||(is_zero(a) && is_zero(b)))
     return false;
 
   const bool sign_a = a.bits >> Float32::SIGN_OFFSET;
@@ -250,7 +499,7 @@ constexpr bool qls(const Float32 a, const Float32 b){
     return a.bits < b.bits;
   // both negative
   if(sign_a && sign_b)
-    return b.bits > a.bits;
+    return b.bits < a.bits;
 
   // a<=0 && b>=0
   return sign_a && !sign_b;
@@ -258,6 +507,8 @@ constexpr bool qls(const Float32 a, const Float32 b){
 constexpr bool qle(const Float32 a, const Float32 b){
   if(is_nan(a) || is_nan(b))
     return false;
+  if(is_zero(a) && is_zero(b))
+    return true;
 
   const bool sign_a = a.bits >> Float32::SIGN_OFFSET;
   const bool sign_b = b.bits >> Float32::SIGN_OFFSET;
@@ -267,7 +518,7 @@ constexpr bool qle(const Float32 a, const Float32 b){
     return a.bits <= b.bits;
   // both negative
   if(sign_a && sign_b)
-    return b.bits >= a.bits;
+    return b.bits <= a.bits;
 
   // a<=0 && b>=0
   return sign_a && !sign_b;
@@ -314,7 +565,6 @@ constexpr bool total_order(const Float32 a, const Float32 b){
 		return true;
 	}
 
-
 	//a)
 	if(qls(a, b))
 		return true;
@@ -330,52 +580,57 @@ constexpr bool total_order(const Float32 a, const Float32 b){
 	return is_sign_minus(a);
 }
 
-constexpr bool total_order_mag(const Float32 a, const Float32 b){
-  return total_order(abs(a), abs(b));
+constexpr Float32 next_up(const Float32 a){
+  if(is_nan(a))
+    return a;
+  if(is_zero(a))
+    return 1;
+  if(is_sign_minus(a))
+    return {a.bits-1};
+  else 
+    return is_inf(a) ? a : Float32{a.bits+1};
 }
+constexpr Float32 next_down(const Float32 a){
+  return neg(next_up(neg(a)));
+}
+constexpr Float32 min_val(const Float32 a, const Float32 b){
+  if(is_nan(a))
+    return a;
+  if(is_nan(b))
+    return b;
+
+  return qls(a,b)? a : b;
+}
+constexpr Float32 max_val(const Float32 a, const Float32 b){
+  if(is_nan(a))
+    return a;
+  if(is_nan(b))
+    return b;
+
+  return qgr(a,b)? a : b;
+}
+// constexpr Float32 reminder(const Float32, const Float32){}
+
 
 
 constexpr Float32 
     Float32::ZERO = Float32{0},
-    Float32::NEG_ZERO = Float32{1u<<(Float32::SIGN_OFFSET)},
-    Float32::INFTY{pack(IEEEFields{false, bit_mask<int32_t>(EXPONENT_SIZE), 0})},
+    Float32::NEG_ZERO = Float32{1u<<Float32::SIGN_OFFSET},
+    Float32::INFTY{bit_mask<int32_t>(EXPONENT_SIZE) << (Float32::EXPONENT_OFFSET)},
     Float32::NEG_INFTY{neg(Float32::INFTY)},
-    Float32::NaN{pack(IEEEFields{false, bit_mask<int32_t>(EXPONENT_SIZE), 1})},
-    Float32::NEG_NaN{pack(IEEEFields{true, bit_mask<int32_t>(EXPONENT_SIZE), 1})},
+    Float32::QNaN{(bit_mask<uint32_t>(EXPONENT_SIZE) << (Float32::EXPONENT_OFFSET)) | (1<<(MANTISSA_SIZE-1))},
+    Float32::SNaN{(bit_mask<uint32_t>(EXPONENT_SIZE) << (Float32::EXPONENT_OFFSET)) | (1<<(MANTISSA_SIZE-2))},
     Float32::MAX{Float32::INFTY.bits - 1},
-    Float32::MIN{Float32::NEG_INFTY.bits - 1};
+    Float32::MIN{neg(Float32::MAX)};
 
 
-int main() {
-  // std::cout << "infinity test:\n" << std::fixed << std::setprecision(30)
-  //           << "inf-1: " <<  std::bit_cast<float>(std::bit_cast<uint32_t>(std::numeric_limits<float>::infinity())-1) << '\n'
-  //           << "max: " << std::numeric_limits<float>::max()  << '\n'
-  //           << "equal: " << std::boolalpha << (std::bit_cast<float>(std::bit_cast<uint32_t>(std::numeric_limits<float>::infinity())-1) == std::numeric_limits<float>::max())
-  //           << std::endl;
-
-  float TEST_FLOATS[] = {0.1f, 0.5f, 1.75f, 10.10f, 0.001f, std::numeric_limits<float>::infinity(), -std::numeric_limits<float>::infinity(), std::numeric_limits<float>::max(), std::numeric_limits<float>::quiet_NaN(), std::numeric_limits<float>::min(), -0.0f, 0.0f};
-  
-  // test pack/unpack
-  for(float f : TEST_FLOATS){
-    Float32 my_float{std::bit_cast<uint32_t>(f)};
-    Float32 unpacked_packed{pack(unpack(my_float))};
-
-    assert(my_float==unpacked_packed && "pack/unpack identity");
-  }
-
-  // test binary operations
-  for(auto a : TEST_FLOATS){
-  for(auto b : TEST_FLOATS){
-    Float32 af{static_cast<uint32_t>(a)};
-    Float32 bf{static_cast<uint32_t>(b)};
-
-    float a_mul_b = a*b;
-    Float32 af_mul_bf = mul(af,bf);
-    Float32 proper_mul{static_cast<uint32_t>(a_mul_b)};
-
-    ASSERT_EQ(proper_mul, af_mul_bf, "Wrong result multiplication\n");
-      // std::cout << "MyImpl: mantissa: " << unpack(af_mul_bf).mantissa << " exponent: " << unpack(af_mul_bf).exponent << std::endl;
-      // std::cout << "BuiltIn: mantissa: " << unpack(proper_mul).mantissa << " exponent: " << unpack(proper_mul).exponent << std::endl;
-  }}
+constexpr bool operator==(const float a, const Float32 b){
+  return std::bit_cast<uint32_t>(a)== b.bits;
 }
+constexpr bool operator!=(const float a, const Float32 b){
+  return !(a == b);
+}
+
+
+
 
--- floating.hpp
+++ floating.hpp
@@ -1,7 +1,10 @@
 #include <bitset>
 #include <cstddef>
 #include <cstdint>
+#include <iostream>
+#include <limits>
 #include <ostream>
+#include <type_traits>
 
 enum class RoundingType{
   ToEven, TowardZero, Positive, Negative, ToAway
@@ -11,9 +14,14 @@ struct Float32;
 struct IEEEFields{
   bool sign_bit;
   // biased exponent
-  int16_t exponent;
+  uint16_t exponent;
   // mantissa with leading 1 (no subnormals)
   uint32_t mantissa;
+
+  friend std::ostream& operator<<(std::ostream& os, const IEEEFields& f){
+    os << "sign: " << f.sign_bit << " exponent: " << f.exponent << "(0b" << std::bitset<12>(f.exponent) << ")" << ", mantissa: " << f.mantissa << "(0b" << std::bitset<24>(f.mantissa) << ")";
+    return os;
+  }
 };
 
 template<typename T> 
@@ -32,8 +40,6 @@ constexpr Float32 pack(IEEEFields fields);
   section 5.3.1
 */
 
-template<typename T, const RoundingType>
-constexpr T round_to_integral(const Float32);
 constexpr Float32 next_up(const Float32);
 constexpr Float32 next_down(const Float32);
 constexpr Float32 min_val(const Float32, const Float32);
@@ -54,8 +60,6 @@ constexpr Float32 mul(const Float32, const Float32);
 constexpr Float32 div(const Float32, const Float32);
 constexpr Float32 sqrt(const Float32);
 constexpr Float32 fused_mul_add(const Float32, const Float32, const Float32);
-template<typename T, const RoundingType>
-constexpr Float32 convert_from_int(T);
 
 /*
   5.4.2 conversion between float and string
@@ -88,37 +92,42 @@ constexpr bool qle(const Float32, const Float32);
 constexpr bool is_754_version_1985 () {return false;}
 constexpr bool is_754_version_2008 () {return false;}
 constexpr bool is_754_version_2019 () {return true;}
+constexpr uint32_t radix(){return 2;}
 
 /* 
   5.7.2 general operations
 */
-enum class cls {sNan, qNan, nInf, nNrm, nZr, pZr, pSbn, pNrm, pInf};
+enum class cls {sNan, qNan, nInf, nSbn, nNrm, nZr, pZr, pSbn, pNrm, pInf};
+constexpr cls  float_class(const Float32);
 constexpr bool is_sign_minus(const Float32);
 constexpr bool is_normal(const Float32);
 constexpr bool is_finite(const Float32);
 constexpr bool is_zero(const Float32);
 constexpr bool is_subnormal(const Float32);
-constexpr bool is_infinite(const Float32);
+constexpr bool is_inf(const Float32);
 constexpr bool is_nan(const Float32);
 constexpr bool is_signaling(const Float32);
-constexpr bool is_canonical(const Float32);
 constexpr bool total_order(const Float32, const Float32);
 constexpr bool total_order_mag(const Float32, const Float32);
 
 struct Float32{
   uint32_t bits;
 
-  static const uint32_t BIAS = 1024;
   static const uint32_t EXPONENT_SIZE=8, MANTISSA_SIZE=23;
-  static const int32_t EXPONENT_BIAS=127;
-  static const int32_t EXPONENT_FIRST_BIT=1<<(EXPONENT_SIZE+2);
+  static const uint32_t EXPONENT_BIAS=127;
+  static const uint32_t EXPONENT_FIRST_BIT=1<<(EXPONENT_SIZE+2);
 
   static const uint32_t TOTAL_SIZE = 1+EXPONENT_SIZE+MANTISSA_SIZE;
   static const uint32_t MANTISSA_OFFSET = 0;
   static const uint32_t EXPONENT_OFFSET = MANTISSA_SIZE;
   static const uint32_t SIGN_OFFSET = EXPONENT_SIZE + MANTISSA_SIZE;
 
-  static const Float32 ZERO, NEG_ZERO, INFTY, NEG_INFTY, NaN, NEG_NaN, MAX, MIN;
+  // implementation details
+  static const uint32_t BIAS = 1<<(EXPONENT_SIZE+2); // 1024
+  static const uint64_t MANTISSA_LEADING_1 = 1 << MANTISSA_SIZE; 
+  static const uint32_t EXPONENT_MAX_BORDER = 1 << (13-1);
+
+  static const Float32 ZERO, NEG_ZERO, INFTY, NEG_INFTY, QNaN, SNaN, MAX, MIN;
 
   constexpr bool operator==(const Float32& other)const{
     return this->bits==other.bits;
@@ -130,44 +139,169 @@ struct Float32{
     return (bool)((bits >> Float32::SIGN_OFFSET) & 1);
   }
 
-  constexpr int16_t get_exponent() const{
-    return static_cast<int16_t>(
-              (bits >> Float32::EXPONENT_OFFSET) & bit_mask<uint16_t>(Float32::EXPONENT_SIZE)
-            ) - Float32::EXPONENT_BIAS;
+  constexpr uint16_t get_exponent() const{
+    // if(((bits >> Float32::EXPONENT_OFFSET) & bit_mask<uint16_t>(Float32::EXPONENT_SIZE)) == 0)
+    //   return 0;
+    return (static_cast<uint16_t>(bits >> Float32::EXPONENT_OFFSET) & bit_mask<uint16_t>(Float32::EXPONENT_SIZE))
+           + Float32::BIAS - Float32::EXPONENT_BIAS;
   }
   constexpr uint32_t get_mantissa() const{
     return ((bits >> Float32::MANTISSA_OFFSET) & bit_mask<uint32_t>(Float32::MANTISSA_SIZE));
   }
-
   friend std::ostream& operator<<(std::ostream& os, const Float32& f){
-    auto a = unpack(f);
-    os << "bits: " << std::bitset<TOTAL_SIZE>(f.bits) << '\n'
-       << "sign: " << a.sign_bit << "exponent: " << a.exponent << ", mantissa: " << a.mantissa;
+    os << std::bit_cast<float>(f.bits) << " (0b" << std::bitset<TOTAL_SIZE>(f.bits) << ")";
     return os;
   }
-  
+  constexpr operator float() const{
+    return std::bit_cast<float>(this->bits);
+  }
+  constexpr Float32(const uint32_t bits)
+    :bits{bits}
+  {}
 };
 
 
 
 constexpr IEEEFields unpack(Float32 f){
-  return IEEEFields{
+  auto fields = IEEEFields{
     .sign_bit=f.get_sign_bit(),
     .exponent=f.get_exponent(),
     .mantissa=f.get_mantissa()
   };
+
+  // std::cout << (fields.exponent-Float32::BIAS+Float32::EXPONENT_BIAS) << std::endl;
+  // if(fields.exponent-Float32::BIAS+Float32::EXPONENT_BIAS == bit_mask<uint16_t>(Float32::EXPONENT_SIZE)){
+  //   fields.exponent = bit_mask<uint16_t>(Float32::EXPONENT_SIZE);
+  //   return fields;
+  // }
+
+  // if exponent equals zero
+  if(fields.exponent == Float32::BIAS - Float32::EXPONENT_BIAS){
+    if(fields.mantissa == 0){
+      fields.exponent = 0;
+      return fields;
+    }
+
+    fields.mantissa <<= 1;
+    // change subnormals to normals
+    while(fields.mantissa < Float32::MANTISSA_LEADING_1){
+      fields.mantissa <<= 1;
+      fields.exponent -= 1;
+    }
+  }
+
+  fields.mantissa |= Float32::MANTISSA_LEADING_1;
+  return fields;
 }
 
-constexpr Float32 pack(IEEEFields fields){
-  return Float32{.bits=(fields.sign_bit << (Float32::SIGN_OFFSET))
-    | ((static_cast<uint32_t>(fields.exponent + Float32::EXPONENT_BIAS) & bit_mask<uint32_t>(Float32::EXPONENT_SIZE)) << Float32::EXPONENT_OFFSET)
-    | ((fields.mantissa & bit_mask<uint32_t>(Float32::MANTISSA_SIZE)) << Float32::MANTISSA_OFFSET)
-    };
+constexpr Float32 pack(const IEEEFields fields){
+  	uint32_t sign_bit = fields.sign_bit;
+  	uint32_t exponent = fields.exponent;
+  	uint32_t mantissa = fields.mantissa;
+
+  	//arbitrary border for overflow detection
+  	if(exponent > Float32::EXPONENT_MAX_BORDER)
+  		return Float32{(sign_bit << Float32::SIGN_OFFSET) | 0};
+  	
+  	if(fields.exponent == (bit_mask<uint16_t>(Float32::EXPONENT_SIZE) + static_cast<uint16_t>(Float32::BIAS) - static_cast<uint16_t>(Float32::EXPONENT_BIAS)))
+  	  return ((mantissa&bit_mask<uint32_t>(Float32::MANTISSA_SIZE))!=0) 
+              ? Float32::QNaN
+  	          : ((sign_bit)
+  	            ? Float32::NEG_INFTY
+  	            : Float32::INFTY);
+
+  	if(exponent > Float32::BIAS + Float32::EXPONENT_BIAS)
+  		return sign_bit ? Float32::MIN : Float32::MAX;
+
+  	if(exponent <= Float32::BIAS - Float32::EXPONENT_BIAS)
+  		exponent--;
+
+  	//convert from extended precision normal to subnormal
+  	while(exponent < Float32::BIAS - Float32::EXPONENT_BIAS
+  	   && mantissa != 0)
+  	{
+  		exponent++;
+  		mantissa >>= 1;
+  	}
+
+  	//if no implicit one, return 0 because extended precision 
+  	if(mantissa == 0) 
+  		return {sign_bit << Float32::SIGN_OFFSET};
+	
+  	return {((sign_bit & 1)  << Float32::SIGN_OFFSET)
+  	     | (((exponent - Float32::BIAS + Float32::EXPONENT_BIAS) & bit_mask<uint32_t>(Float32::EXPONENT_SIZE)) << Float32::EXPONENT_OFFSET)
+  	     | (  mantissa & bit_mask<uint32_t>(Float32::MANTISSA_SIZE))};
+
+
+  // return Float32{.bits=(fields.sign_bit << (Float32::SIGN_OFFSET))
+  //   | ((static_cast<uint32_t>(fields.exponent + Float32::EXPONENT_BIAS) & bit_mask<uint32_t>(Float32::EXPONENT_SIZE)) << Float32::EXPONENT_OFFSET)
+  //   | ((fields.mantissa & bit_mask<uint32_t>(Float32::MANTISSA_SIZE)) << Float32::MANTISSA_OFFSET)
+  //   };
 }
 
 static_assert(Float32::TOTAL_SIZE == sizeof(Float32)*8);
 
 
+template<typename T, const RoundingType=RoundingType::TowardZero>
+constexpr Float32 convert_from_int(T i){
+  static_assert(std::is_integral<T>() && std::is_signed<T>() && "Converion type must be signed integral");
+  using uT = std::make_unsigned_t<T>;
 
+  if(i == 0) 
+    return Float32::ZERO;
+  
+  // constexpr size_t INT_SIZE = 8*sizeof(T);
 
-constexpr Float32 int_to_float(int32_t integer);
+  bool sign = i<0;
+  i = std::abs(i);
+  
+  uT mantissa = static_cast<uT>(i);
+  uint32_t exponent = Float32::EXPONENT_BIAS + Float32::MANTISSA_SIZE;
+
+  static_assert(static_cast<T>(Float32::MANTISSA_LEADING_1) < bit_mask<T>(Float32::MANTISSA_SIZE+1));
+  while(mantissa > bit_mask<T>(Float32::MANTISSA_SIZE+1)){
+    mantissa >>= 1;
+    exponent += 1;
+  }
+    while(mantissa < static_cast<T>(Float32::MANTISSA_LEADING_1)){
+      mantissa <<= 1;
+      exponent -= 1;
+  }
+
+  exponent <<= Float32::EXPONENT_OFFSET;
+  mantissa &= bit_mask<T>(Float32::MANTISSA_SIZE);
+
+  return Float32{sign<<Float32::SIGN_OFFSET | static_cast<uint32_t>(exponent) | static_cast<uint32_t>(mantissa)};
+}
+
+template<typename T, const RoundingType=RoundingType::TowardZero>
+constexpr T round_to_int(const Float32 fa){
+  static_assert(std::is_integral<T>() && std::is_signed<T>() && "Converion type must be signed integral");
+
+  constexpr size_t INT_SIZE = 8*sizeof(T);
+  if(is_nan(fa))
+    return std::numeric_limits<T>::min();
+
+  if(is_inf(fa)){
+    return is_sign_minus(fa)
+         ? std::numeric_limits<T>::min()
+         : std::numeric_limits<T>::min();
+  }
+
+  auto a = unpack(fa);
+  T mantissa = static_cast<T>(a.mantissa);
+
+  if(a.exponent < Float32::BIAS)
+    mantissa = 0;
+  else if(a.exponent >= Float32::BIAS + INT_SIZE-1)
+    return a.sign_bit
+         ? std::numeric_limits<T>::min()
+         : std::numeric_limits<T>::max();
+
+  if(a.exponent > Float32::BIAS + Float32::MANTISSA_SIZE)
+    mantissa <<= a.exponent - Float32::BIAS - Float32::MANTISSA_SIZE;
+  else
+    mantissa >>= Float32::MANTISSA_SIZE - (a.exponent - Float32::BIAS);
+
+  return a.sign_bit ? -(mantissa) : mantissa;
+}
